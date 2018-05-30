c8b0593973a01d660739be8c638a314999f3be4f
prgr <andrew@bks.tv>
2018-05-31 00:06:38 +0300

    _example and docker-compose for redis

--- _example/main.go
+++ _example/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	mem "github.com/prgra/storage/mem"
+	redis "github.com/prgra/storage/redis"
+)
+
+func main() {
+	mem.Push("test", "hohoho")
+	v, ok := mem.Get("test")
+	if ok {
+		fmt.Println(v)
+	}
+	redis.New("127.0.0.1:6379", "", 0)
+	err := redis.Push("hello", "World")
+	if err != nil {
+		panic(err)
+	}
+
+	i, err := redis.Get("hello")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(i)
 }
